a28d79b584c37664b63d6603414466248f5ed5cf
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-09-17 13:52:00 -0300

    feat; adiconando  API Open Weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState , useRef} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
  const inputRef = useRef();
 
-  function searchCity() {
-    console.log(inputRef.current.value);
-  }
+ async function searchCity() {
+     const city = inputRef.current.value;
+     const key = "4868af3a3caa3cc3a9fe424a2ebea01d";
+     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
+
+     const data = await axios.get(url)
+       console.log(data);
+     
+  
+    }
 
   return (
     <div>
