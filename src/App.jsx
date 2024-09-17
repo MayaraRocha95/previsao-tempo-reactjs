7fdfaa60f91d2ee62980f86713f5d6d6069dd3cb
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-09-17 13:28:39 -0300

    criando o projeto do zero

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState , useRef} from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+ const inputRef = useRef();
+
+  function searchCity() {
+    console.log(inputRef.current.value);
+  }
+
+  return (
+    <div>
+      <h1>ElasClub Previsão do Tempo</h1>
+      <input ref={inputRef} type="text" placeholder="Digite o nome da cidade" />
+      <button onClick={searchCity}>Buscar</button>
+    </div>
+  );
+}
+
+export default App;
